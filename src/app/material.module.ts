ccebfa69e10266028e376bd24aa2d0ba9198f14f
James Kinyua <jameskinyua590@gmail.com>
2018-09-20 10:45:12 +0300

    chore(user experience): Generate components for user experience

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core'
-import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material'
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from '@angular/material'
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTooltipModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTooltipModule],
   declarations: [],
 })
 export class MaterialModule {}
--- src/app/pos/pos.module.ts
+++ src/app/pos/pos.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
+import { NgModule } from '@angular/core'
 import { PosRoutingModule } from './pos-routing.module'
 
 @NgModule({
